84330dfb1747b4d66a028f90f1ee6e321c153944
vtkiet <it.tuankiet58@gmail.com>
2023-05-12 23:30:10 +0700

    add lesson 3

---
+++ lesson-3/main.js
@@ -0,0 +1,37 @@
+/**
+Giới thiệu về toán tử trong Javascript
+1. Toán tử số học - Arithmetic
+2. Toán tử gắn 2 . Assignment
+3. Toán tử so sánh – Comparison
+4. Toán tử logic – Logical
+
+*/
+
+var a = 1 * 2;
+
+var fullName = "Vo Tuan Kiet"
+
+var b = 1;
+var c = 2;
+if (b == c){
+    console.log('Dung');
+}
+else{
+    console.log('sai');
+}
+
+var d = 1;
+var e = 2;
+if ( a > 0 && b > 0){
+    console.log('a va b lon hon 0');
+}
+
+
+var p = a + b;
+console.log(p);
+var p = a - b;
+console.log(p);
+var p = a * b;
+console.log(p);
+var p = a / b;
+console.log(p);
